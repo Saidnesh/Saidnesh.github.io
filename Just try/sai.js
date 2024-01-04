009684a1854f723e954be4f6dcfff848d4f0a99b
Saidnesh <143603631+Saidnesh@users.noreply.github.com>
2024-01-04 23:52:02 +0800

    Add files via upload

---
+++ Just try/sai.js
@@ -0,0 +1,8 @@
+ function validateForm() {
+  alert('The form has been submitted');
+  return true;
+ }
+    
+    function resetForm() {
+     document.getElementById('registrationForm').reset();
+}
